fff2ee6ce20def63b2831b95845b03ea1e240c66
MrStepus <steptale@gmail.com>
2026-01-05 23:04:42 +0300

    v26.0.1.2 - Player movement
    Я сделал управление для игрока, Ура победа

---
+++ Assets/assets/Script/Player/PlayerInput.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Vector3 _direction;
+
+    private void OnEnable()
+    {
+        InputManager.Instance.actions.Player.Move.performed += OnMove;
+        InputManager.Instance.actions.Player.Move.canceled += OnMoveStop;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.actions.Player.Move.performed -= OnMove;
+        InputManager.Instance.actions.Player.Move.canceled -= OnMoveStop;
+    }
+
+    private void Update()
+    {
+        Move(_direction);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector3>();
+        Debug.Log($"Move: {_direction}"); 
+    }
+
+    public void OnMoveStop(InputAction.CallbackContext context)
+    {
+        _direction = Vector3.zero; 
+    }
+
+    private void Move(Vector2 directionMove)
+    {
+        Vector3 movement = new Vector3(_direction.x, 0f, _direction.z) * moveSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
